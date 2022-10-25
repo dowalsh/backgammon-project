ea6473831a472b4a959089d06885a2d29b78594e
samball0 <sam.lynch1@ucdconnect.ie>
2022-10-25 17:15:37 +0100

    Adding Point class

---
+++ src/backgammon/Point.java
@@ -0,0 +1,30 @@
+package backgammon;
+
+/**
+ * This program is the Point class
+ * @author Sam Lynch
+ *
+ */
+
+/**
+ * A {@code Point} is an extension of the BoardSpace class, representing a point on the backgammon board
+ */
+public class Point extends BoardSpace{
+	
+	/**
+	 * Constructor for this class
+	 */
+	public Point() {
+	}
+	
+	public boolean canPlace(Checker start) {
+		//Needs to be updated
+		return true;
+	}
+	
+	public boolean canTake() {
+		//Needs to be updated
+		return true;
+	}
+
+}
