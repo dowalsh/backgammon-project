fc8fb072400bb311cd4152226d62f9e389e33eee
carnivorousmole <dylanwitsend@gmail.com>
2022-10-25 18:06:06 +0100

    added BackgammonBoardView class

---
+++ src/backgammon/BackgammonBoardView.java
@@ -0,0 +1,20 @@
+package backgammon;
+
+/**
+ * This program is the BackgammonBoardView class
+ * @author dylan
+ *
+ */
+
+/**
+ * The BackgammonBoardView Class is used to display a game of Backgammon in the console
+ */
+public class BackgammonBoardView {
+
+	public static void print(BackgammonBoard board) {
+		
+		System.out.println("Hello I'm Backgammon!");
+
+	}
+
+}
