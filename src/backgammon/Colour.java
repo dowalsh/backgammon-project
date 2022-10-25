9d5835dc1f5244f7524732b405f428ba263a236e
carnivorousmole <dylanwitsend@gmail.com>
2022-10-25 16:16:54 +0100

    added Colour enum

---
+++ src/backgammon/Colour.java
@@ -0,0 +1,16 @@
+package backgammon;
+
+
+/**
+ * This program is the Colour enum
+ * @author dylan
+ *
+ */
+
+/**
+ * the Colour enum represents the two colours in backgammon
+ */
+public enum Colour {
+	BLACK,
+	WHITE;
+}
