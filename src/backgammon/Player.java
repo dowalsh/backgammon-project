636a29cf1ec4fc31506f84aaba89dae4b51976a1
samball0 <sam.lynch1@ucdconnect.ie>
2022-10-28 17:53:02 +0100

    Updating Javadoc comments for Player class

--- src/backgammon/Player.java
+++ src/backgammon/Player.java
@@ -1,23 +1,41 @@
 package backgammon;
-
 import java.util.Scanner;
 
+/**
+ * This program is the Player class
+ * @author Sam Lynch
+ *
+ */
+
+/**
+ * A {@code Player} represents a player
+ */
 public class Player {
 	private static Scanner in = new Scanner(System.in);
 	private String Name;
 	private Colour PlayersColour;
 
+	/**
+	 * Constructor for this class. Sets name and colour of player.
+	 * @param colour Colour of player.
+	 */
 	public Player(Colour colour) {
-		this.Name = setName();
+		setName();
 		this.PlayersColour = colour;
 	}
 	
-	public String setName() {
+	/**
+	 * Prompts the player to enter their name and returns the name entered.
+	 */
+	public void setName() {
 		System.out.print("Enter the player name: ");
 		String name = in.nextLine();
-		return name;
+		this.Name = name;
 	}
 
+	/**
+	 * Overwrites inbuilt toString() method for this class. 
+	 */
 	public String toString() {
 		return "Name: "+Name+"\nColour: "+PlayersColour;
 	}
