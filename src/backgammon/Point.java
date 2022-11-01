2e4fbb479e40beaf6daf772c2babff908c424c1d
carnivorousmole <dylanwitsend@gmail.com>
2022-11-01 17:31:59 +0000

    updated constructor to add checkers to the point depending on its index

--- src/backgammon/Point.java
+++ src/backgammon/Point.java
@@ -7,57 +7,86 @@ package backgammon;
  */
 
 /**
- * A {@code Point} is an extension of the BoardSpace class, representing a point on the backgammon board
+ * A {@code Point} is an extension of the BoardSpace class, representing a point
+ * on the backgammon board
  */
-public class Point extends BoardSpace{
-	
+public class Point extends BoardSpace {
+
 	/**
 	 * Constructor for this class
 	 */
-	public Point(int number) {
+	public Point(int white_index) {
 		// unsure if this is correct (Looks Good to me(Sam))
-		// basically  using the odd/even rule to colour them
-		// Do we want to add starting checkers in here?(2 white in 1, 5 black in 6, 3 black in 8, 5 white in 12, 
-		// 5 black in 13, 3 white in 17, 5 white in 19, 2 black in 24) Maybe separate constructor? Maybe in Board class?
-		if(number%2 == 0) {
+		// basically using the odd/even rule to colour them
+		// Do we want to add starting checkers in here?(2 white in 1, 5 black in 6, 3
+		// black in 8, 5 white in 12,
+		// 5 black in 13, 3 white in 17, 5 white in 19, 2 black in 24) Maybe separate
+		// constructor? Maybe in Board class?
+		if (white_index % 2 == 0) {
 			this.setColour(Colour.BLACK);
-		}else {
+		} else {
 			this.setColour(Colour.WHITE);
 		}
+
+		switch (white_index) {
+		case 24:
+			addNewCheckers(2,Colour.WHITE);
+			break;
+		case 13:
+			addNewCheckers(5,Colour.WHITE);
+			break;
+		case 8:
+			addNewCheckers(3,Colour.WHITE);
+			break;
+		case 6:
+			addNewCheckers(5,Colour.WHITE);
+			break;	
+		case 25-24:
+			addNewCheckers(2,Colour.BLACK);
+			break;
+		case 25-13:
+			addNewCheckers(5,Colour.BLACK);
+			break;
+		case 25-8:
+			addNewCheckers(3,Colour.BLACK);
+			break;
+		case 25-6:
+			addNewCheckers(5,Colour.BLACK);
+			break;	
+		}
 	}
-	
+
 	@Override
 	public boolean canPlace(Checker start) {
 		boolean Place = false;
-		if(this.isEmpty()) {
+		if (this.isEmpty()) {
 			Place = true;
-		}
-		else if(start.getColour().equals(getTopChecker().getColour())) {
+		} else if (start.getColour().equals(getTopChecker().getColour())) {
 			Place = true;
-		}
-		else if(this.getNumCheckers() == 1) {
+		} else if (this.getNumCheckers() == 1) {
 			Place = true;
 		}
 		return Place;
 	}
-	
+
 	@Override
 	public boolean canTake() {
 		boolean Take = true;
-		if(this.isEmpty()) {
+		if (this.isEmpty()) {
 			Take = false;
 		}
 		return Take;
 	}
-	
+
 	/**
 	 * Checks if moving a checker onto this point will result in a hit.
+	 * 
 	 * @param start Checker to move onto point.
 	 * @return Whether or not a hit occurs.
 	 */
 	public boolean isAHit(Checker start) {
 		boolean Hit = false;
-		if(this.getNumCheckers()==1 && !start.getColour().equals(getTopChecker().getColour())) {
+		if (this.getNumCheckers() == 1 && !start.getColour().equals(getTopChecker().getColour())) {
 			Hit = true;
 		}
 		return Hit;
