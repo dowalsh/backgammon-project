bfbfac455f130a8aec568e11405e80cea56e31fc
carnivorousmole <dylanwitsend@gmail.com>
2022-11-01 16:57:12 +0000

    added ToString method

--- src/backgammon/Checker.java
+++ src/backgammon/Checker.java
@@ -7,20 +7,22 @@ package backgammon;
  */
 
 /**
- * A {@code Checker} object represents a single checker in the game of backgammon
+ * A {@code Checker} object represents a single checker in the game of
+ * backgammon
  */
 public class Checker {
-	
+
 	private Colour colour;
 
 	/**
 	 * Constructor for class
+	 * 
 	 * @param colour the colour of this checker
 	 */
 	public Checker(Colour colour) {
 		this.colour = colour;
 	}
-	
+
 	/**
 	 * @return the colour of this checker
 	 */
@@ -28,4 +30,16 @@ public class Checker {
 		return colour;
 	}
 
+	@Override	
+	public String toString() {
+		String checkerString = new String();
+		
+		if(this.colour == Colour.BLACK) {
+			checkerString = "●";
+		}else {
+			checkerString = "○";
+		}
+				
+		return checkerString;
+	}
 }
