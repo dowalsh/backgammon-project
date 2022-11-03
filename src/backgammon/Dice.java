1193b220faf1e3e13b7b4addb8cfc0d8d4b67e7c
carnivorousmole <dylanwitsend@gmail.com>
2022-11-03 17:49:33 +0000

    widened dice display to be more square-like

--- src/backgammon/Dice.java
+++ src/backgammon/Dice.java
@@ -11,63 +11,64 @@ package backgammon;
  */
 public class Dice {
 	private final static double SIDES_ON_DICE = 6.0;
-	
+
 	/**
 	 * Constructor for this class.
 	 */
 	public Dice() {
-		
+
 	}
-	
+
 	/**
 	 * Rolls the dice.
+	 * 
 	 * @return The result of the roll
 	 */
-	public final static int roll () {
-		double roll = Math.random()*SIDES_ON_DICE+1.0;
+	public final static int roll() {
+		double roll = Math.random() * SIDES_ON_DICE + 1.0;
 		return (int) roll;
 	}
-	
+
 	public final static String toString(int results[]) {
-		
-		String indent = " ".repeat(17);
+
+		String indent = " ".repeat(15);
 		String gap = " ".repeat(5);
 
 		String[][] diceFace1 = getDiceFace(results[0]);
 		String[][] diceFace2 = getDiceFace(results[1]);
 
 		final StringBuilder formatString = new StringBuilder("");
-		//header line
+		// header line
+		formatString.append("\n");
 		formatString.append(indent);
-		formatString.append("+-----+"+ gap+ "+-----+\n");
-		for(int i = 0; i<3;i++) {
+		formatString.append("+-------+" + gap + "+-------+\n");
+		for (int i = 0; i < 3; i++) {
 			formatString.append(indent);
-			formatString.append("|");
-			for(int j = 0; j<3;j++) {
+			formatString.append("| ");
+			for (int j = 0; j < 3; j++) {
 				formatString.append(diceFace1[i][j]);
-				if(j<2) formatString.append(" ");
+				formatString.append(" ");
 			}
-			formatString.append("|"+gap+"|");
-			for(int j = 0; j<3;j++) {
+			formatString.append("|" + gap + "| ");
+			for (int j = 0; j < 3; j++) {
 				formatString.append(diceFace2[i][j]);
-				if(j<2) formatString.append(" ");
+				formatString.append(" ");
 			}
 			formatString.append("|\n");
 		}
 		formatString.append(indent);
-		formatString.append("+-----+"+gap+"+-----+");
+		formatString.append("+-------+" + gap + "+-------+\n");
 
-		
 		return formatString.toString();
 	}
-	
+
 	private final static String[][] getDiceFace(int n) {
-		
-		String[][] diceFace = { {" "," "," "},{" "," "," "},{" "," "," "} };
-		
+
+		String[][] diceFace = { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
+
 		String dot = "o";
-		
-		switch(n) {
+
+		switch (n) {
 		case 1:
 			diceFace[1][1] = dot;
 			break;
@@ -102,8 +103,7 @@ public class Dice {
 			diceFace[2][2] = dot;
 			break;
 		}
-		
+
 		return diceFace;
 	}
 }
-
